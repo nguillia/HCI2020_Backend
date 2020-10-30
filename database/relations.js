d2c14c4aa13031276643fe47cbe7dd822542a7f0
Niels Guilliams <niels.guilliams@telenet.be>
2020-10-30 17:42:48 +0100

    First commit

---
+++ database/relations.js
@@ -0,0 +1,7 @@
+const { sequelize } = require('./init');
+
+console.log('DB Relations added.');
+
+// Sync
+// sequelize.sync();
+// console.log('DB Synced.');
---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+/* NPM PACKAGES */
+const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
+const https = require('https');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+/* END NPM PACKAGES */
+
+/* CUSTOM PACKAGES */
+const database = require('./database/init');
+/* END CUSTOM PACKAGES */
+
+/* VARIABLES */
+const app = express();
+const port = process.env.PORT || 3001;
+/* END VARIABLES */
+
+/* HEROKU MAGIC */
+// Keep Alive - Check every 15 minutes
+// setInterval(function () {
+//   console.log(new Date(Date.now()).toLocaleString(), 'Keeping app awake.');
+//   https.get('https://lnt-mail-backend.herokuapp.com/');
+// }, 15 * 60 * 1000);
+/* END HEROKU MAGIC */
+
+/* MIDDLEWARE */
+app.options('*', cors());
+app.use(express.json());
+app.use(helmet());
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+/* END MIDDLEWARE */
+
+/* ROUTES */
+require('./routes/routes')(app);
+
+/* FUNCTIONS */
+database.init();
+
+/* LISTEN */
+app.listen(port, () => console.log(`Server up and running on port ${port}.`));
